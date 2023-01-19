d9e6f72c2eaa0610f58f416719e3f79049906242
Rene Mark Barrow <72480653+rb-creator@users.noreply.github.com>
2023-01-19 21:58:00 +0000

    Project Complete
    1. Save, Load and Delete JSON functions.
    2. Clear High Score button added.
    3. Title Text added
    4. HighScore player name and Highscore saved in static class MainManager

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -12,6 +12,7 @@ using UnityEditor;
 public class MenuUIHandler : MonoBehaviour
 {
     public TMP_InputField PlayerName;
+    public TMP_Text HighScoreText;
     
 
     public void PlayerNameChosen(string playerName)
@@ -22,7 +23,7 @@ public class MenuUIHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        HighScoreText.text = $"High Score : {MainManager.Instance.HighScorePlayerName} : {MainManager.Instance.HighScore}";
     }
 
     public void StartNew()
@@ -32,11 +33,21 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
+        MainManager.Instance.SaveHighScore();
+
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
         Application.Quit();
 #endif
+    }
+
+    public void ClearHighScore()
+    {
+        MainManager.Instance.HighScorePlayerName= null;
+        MainManager.Instance.HighScore = 0;
+        HighScoreText.text = $"High Score : {MainManager.Instance.HighScorePlayerName} : {MainManager.Instance.HighScore}";
+        MainManager.Instance.ClearHighScore();
     }
 
     // Update is called once per frame
